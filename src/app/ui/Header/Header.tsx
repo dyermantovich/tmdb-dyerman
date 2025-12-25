b1dfecdf8e65a4bae4a100fe0eeec56135e6b4bc
dyermantovich <dyermantovich@gmail.com>
2025-12-25 10:48:21 +0100

    style: add styles for Header.tsx

--- src/app/ui/Header/Header.tsx
+++ src/app/ui/Header/Header.tsx
@@ -1,17 +1,32 @@
 import { NavLink } from 'react-router';
 import { Path } from '@/common/types';
 import { Logo, SwitchTheme } from '@/app/ui';
+import s from './Header.module.css';
 
 export const Header = () => (
-  <div>
-    <NavLink to={Path.Main}>
+  <header className={s.header}>
+    <NavLink className={s.brand} to={Path.Main}>
       <Logo />
     </NavLink>
-    <NavLink to={Path.Main}>Main</NavLink>
-    <NavLink to={Path.CategoryMovies}>Category Movies</NavLink>
-    <NavLink to={Path.FilteredMovies}>Filtered Movies</NavLink>
-    <NavLink to={Path.Search}>Search</NavLink>
-    <NavLink to={Path.Favorites}>Favorites</NavLink>
-    <SwitchTheme />
-  </div>
+    <nav className={s.nav}>
+      <NavLink className={s.link} to={Path.Main}>
+        Main
+      </NavLink>
+      <NavLink className={s.link} to={Path.CategoryMovies}>
+        Category Movies
+      </NavLink>
+      <NavLink className={s.link} to={Path.FilteredMovies}>
+        Filtered Movies
+      </NavLink>
+      <NavLink className={s.link} to={Path.Search}>
+        Search
+      </NavLink>
+      <NavLink className={s.link} to={Path.Favorites}>
+        Favorites
+      </NavLink>
+    </nav>
+    <div className={s.actions}>
+      <SwitchTheme />
+    </div>
+  </header>
 );
