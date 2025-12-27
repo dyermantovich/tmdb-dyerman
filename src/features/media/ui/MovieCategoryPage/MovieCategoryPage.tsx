18ae6f0b4beec14014ed7bb9f135c47828c790a9
dyermantovich <dyermantovich@gmail.com>
2025-12-27 14:39:49 +0100

    style: make design for MovieCategoryPage.tsx

--- src/features/media/ui/MovieCategoryPage/MovieCategoryPage.tsx
+++ src/features/media/ui/MovieCategoryPage/MovieCategoryPage.tsx
@@ -1,12 +1,29 @@
 import { NavLink, Outlet } from 'react-router';
 import { Path } from '@/common/types';
+import s from './MovieCategoryPage.module.css';
 
 export const MovieCategoryPage = () => (
-  <div>
-    <NavLink to={Path.PopularMovies}>Popular Movies</NavLink>
-    <NavLink to={Path.TopRatedMovies}>Top Rated Movies</NavLink>
-    <NavLink to={Path.UpcomingMovies}>Upcoming Movies</NavLink>
-    <NavLink to={Path.NowPlayingMovies}>Now playing Movies</NavLink>
-    <Outlet />
-  </div>
+  <section className={s.page}>
+    <div className={s.header}>
+      <h2 className={s.title}>Category Movies</h2>
+      <p className={s.subtitle}>Discover what’s trending, top rated, and upcoming.</p>
+    </div>
+    <nav className={s.nav}>
+      <NavLink className={s.link} to={Path.PopularMovies}>
+        Popular
+      </NavLink>
+      <NavLink className={s.link} to={Path.TopRatedMovies}>
+        Top Rated
+      </NavLink>
+      <NavLink className={s.link} to={Path.UpcomingMovies}>
+        Upcoming
+      </NavLink>
+      <NavLink className={s.link} to={Path.NowPlayingMovies}>
+        Now Playing
+      </NavLink>
+    </nav>
+    <div className={s.content}>
+      <Outlet />
+    </div>
+  </section>
 );
